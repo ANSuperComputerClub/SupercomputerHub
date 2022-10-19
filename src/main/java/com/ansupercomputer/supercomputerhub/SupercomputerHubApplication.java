5ae9f4e3addb9d249ec005cd7f6892c95bfcfd97
Arnab Ghosh <arnabcare21@gmail.com>
2022-10-19 10:44:33 -0700

    Switch from Maven to Gradle

---
+++ src/main/java/com/ansupercomputer/supercomputerhub/SupercomputerHubApplication.java
@@ -0,0 +1,13 @@
+package com.ansupercomputer.supercomputerhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupercomputerHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupercomputerHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ansupercomputer/supercomputerhub/SupercomputerHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ansupercomputer.supercomputerhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupercomputerHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
